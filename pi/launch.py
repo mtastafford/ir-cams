dfdd09ddb28736bb18a13e9275791b87f42600ec
mtastafford <mark@themail.io>
2017-05-27 20:36:21 +0000

    Change websocketserver to class. Add launch script

---
+++ pi/launch.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import server
+
+myserver = server.websocketserver('0.0.0.0')
--- pi/server.py
+++ pi/server.py
@@ -1,27 +1,36 @@
 import json
 from websocket_server import WebsocketServer
 
+class websocketserver:
+
+        cams = {}
+        beacons = {}
+        users = {}
+        
+        port = 8001
+
 # Called for every client connecting (after handshake)
-def new_client(client, server):
-	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
-        server.send_message(client, str(client['id']))
+        def new_client(self, client, server):
+	        print("New client connected and was given id %d" % client['id'])
+	        server.send_message_to_all("Hey all, a new client has joined us")
+                server.send_message(client, str(client['id']))
 
 # Called for every client disconnecting
-def client_left(client, server):
-	print("Client(%d) disconnected" % client['id'])
+        def client_left(self, client, server):
+	        print("Client(%d) disconnected" % client['id'])
 
 # Called when a client sends a message
-def message_received(client, server, message):
-	if len(message) > 1000:
-		message = message[:1000]+'..'
+        def message_received(self, client, server, message):
+	        if len(message) > 1000:
+		        message = message[:1000]+'..'
 	#print("Client(%d) said: %s" % (client['id'], message))
-        obj = json.loads(message);
-        print(obj);
+                obj = json.loads(message);
+                print(obj);
+                print(type(obj));
 
-PORT=8001
-server = WebsocketServer(PORT, host='0.0.0.0')
-server.set_fn_new_client(new_client)
-server.set_fn_client_left(client_left)
-server.set_fn_message_received(message_received)
-server.run_forever()
+        def __init__(self, host='0.0.0.0'):
+                self.server = WebsocketServer(self.port, host)
+                self.server.set_fn_new_client(self.new_client)
+                self.server.set_fn_client_left(self.client_left)
+                self.server.set_fn_message_received(self.message_received)
+                self.server.run_forever()
